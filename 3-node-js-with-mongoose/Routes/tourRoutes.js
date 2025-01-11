6f618254af0144808eaa300ae1b9a101116521ea
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2025-01-11 01:58:57 +0100

    Define tourRoutes

--- 3-node-js-with-mongoose/Routes/tourRoutes.js
+++ 3-node-js-with-mongoose/Routes/tourRoutes.js
@@ -4,7 +4,26 @@ const tourController = require('./../Controllers/tourControllers')
 const router = express.Router()
 
 router
+    .route('/top-5-cheap')
+    .get(tourController.aliaTopTours, tourController.getAllTours)
+    
+router
+    .route('/tour-stats')
+    .get(tourController.getTourStats)
+
+.router
+    .route('monthly-plan/:year')
+    .get(tourController.getMonthlyPlan)
+
+router
     .route('/')
     .get(tourController.getAllTours)
+    .post(tourController.createTour)
+
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour)
 
 module.exports = router
