81455c137427e31d86c6ef0443d2e892c5d5d3b0
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2025-01-17 01:29:32 +0100

    Installed/used node debugger for error handling

--- 3-node-js-with-mongoose/Models/tourModel.js
+++ 3-node-js-with-mongoose/Models/tourModel.js
@@ -11,7 +11,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         maxLength: [40, 'A tour name must have less or equal to 40 characters'],
-        minLength: [10, 'A tour name must have less or equal to 10 characters']
+        minLength: [10, 'A tour name must have less or equal to 10 characters'],
     },
     // rating: {
     //     type: Number,
