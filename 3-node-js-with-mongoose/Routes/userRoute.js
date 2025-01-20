39b88f71552c3625ebfd553418559ce55999443e
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2025-01-20 23:01:34 +0100

    Added other route to userRoute

--- 3-node-js-with-mongoose/Routes/userRoute.js
+++ 3-node-js-with-mongoose/Routes/userRoute.js
@@ -5,13 +5,16 @@ const authController = require('./../Controllers/authController')
 // parsing incoming request
 const router = express.Router()
 
+// User Route
 router.route('/')
     .get(userController.getAllUsers)
     .post(userController.getAllUsers)
 
-// router
-//     route('/:id')
-//     .get(userController.getAllUsers)
+router
+    .route('/:id')
+    .get(userController.getUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser)
 
 
 module.exports = router
