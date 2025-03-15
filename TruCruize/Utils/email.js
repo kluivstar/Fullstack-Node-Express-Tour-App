a7e2948c150fb62555116ca5a24bd8e99b63ef17
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2025-03-15 03:23:56 +0100

    Building a complex email handler - installed html-to-text - create Welcome.pug

--- TruCruize/Utils/email.js
+++ TruCruize/Utils/email.js
@@ -1,38 +1,61 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (option)=> {
-    // 1. Create Transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: false,
-        auth:{
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        },
-        
-        logger: true,
-        debug: true
-    });
-
-    // 2. Define Email Options
-    const emailOptions = {
-        from: 'Sins support<support@sins.com>',
-        to: option.email,
-        subject: option.subject,
-        text: option.message
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `X Support <${process.env.EMAIL_FROM}>`
     }
-    // 3. Verify connection configuration - optional
-    transporter.verify(function (error, success) {
-        if (error) {
-        console.log(error);
-        } else {
-        console.log("Server is ready to take our messages.");
-        }
-    });
-    
-    // 4. Actually send the mail
-    await transporter.sendMail(emailOptions)
-};
 
-module.exports = sendEmail;
+    newTransport() {
+        if (process.env.NODE_ENV === 'production'){
+            return nodemailer.createTransport({
+                service: 'SendGrid',
+                auth: {
+                    user: process.env.SENDGRID_USERNAME,
+                    pass: process.env.SENDGRID_PASSWORD
+                }
+            })
+        }
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: false,
+            auth:{
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
+            },
+        })
+    }
+    
+    // Send the actual email
+    async send(template, subject){
+        // 1. Render HTML based on a pug template
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        })
+    
+        // 2. Define Email Options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html)
+        }
+        // 3. Create a transport and send email
+        await this.newTransport().sendMail(mailOptions)
+    }
+    async sendWelcome(){
+        await this.send('Welcome', 'Welcome to the Trucruize family')
+    }
+    async sendPasswordReset(){
+        await this.send('passwordReset', 'Your password reset token (Valid for 10 minutes)')
+    }
+}
+
